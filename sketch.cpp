1d71a798f92d00e71487fd3eeb7a5c7d2c901fe1
Sourav Chakraborty <souravzzz@gmail.com>
2012-12-24 13:32:14 +0530

    fixed indentation

--- sketch.cpp
+++ sketch.cpp
@@ -15,19 +15,19 @@ using namespace std;
 class sketch
 {
 public:
-	sketch(int r, int c);
-	~sketch();
+    sketch(int r, int c);
+    ~sketch();
     int insert(int key, int amount=1);
     int remove(int key, int amount=1);
-	int frequency(int);
+    int frequency(int);
     void normalize();
-	void print();
+    void print();
 
 private:
     int row, col;
-	int **matrix;
-	int *A, *B;
-	int hash(int key, int i);
+    int **matrix;
+    int *A, *B;
+    int hash(int key, int i);
 };
 
 sketch::sketch(int r, int c) 
